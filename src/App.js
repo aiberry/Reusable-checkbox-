958a1d32ce454d360c20b4b8580ffc35dcc12e67
Aleex <aleexpost@gmail.com>
2020-03-30 18:24:31 +0300

    Created Checkbox component  with hover and focus

--- src/App.js
+++ src/App.js
@@ -3,40 +3,50 @@ import styles from "./styles/App.module.css";
 import Checkbox from "./Checkbox.js";
 
 export default function App() {
-  const [targetCheckboxState, setTargetCheckboxState] = useState({ isEnabled: true, isChecked: true });
-  const [enableCheckboxState, setEnableCheckboxState] = useState({ isChecked: false });
-  
-  const onHandleDisable = (e) => {
+  const [targetCheckboxState, setTargetCheckboxState] = useState({
+    isEnabled: true,
+    isChecked: true
+  });
+  const [enableCheckboxState, setEnableCheckboxState] = useState({
+    isChecked: false
+  });
+
+  const onHandleDisable = e => {
     setTargetCheckboxState({
-        ...targetCheckboxState,
-        isEnabled: !targetCheckboxState.isEnabled
-      });
-    setEnableCheckboxState({ ...enableCheckboxState,
-        isChecked: !enableCheckboxState.isChecked})
-  }
+      ...targetCheckboxState,
+      isEnabled: !targetCheckboxState.isEnabled
+    });
+    setEnableCheckboxState({
+      ...enableCheckboxState,
+      isChecked: !enableCheckboxState.isChecked
+    });
+  };
   return (
     <div className={styles.wrap}>
       <h1>Checkboxes</h1>
-      
-        <Checkbox
-          name="Target checkbox"
-          isEnabled={targetCheckboxState.isEnabled}
-          isFocused={false}
-          isHovered={false}
-          isChecked={targetCheckboxState.isChecked}
-          handlePress={() =>
-            setTargetCheckboxState({
-              ...targetCheckboxState,
-              isChecked: !targetCheckboxState.isChecked
-            })
-          }
-        />
-
       <Checkbox
-          name="Disable"
-          isChecked={enableCheckboxState.isChecked}
-          handlePress={onHandleDisable}
-        />
+        name="Target checkbox"
+        isEnabled={targetCheckboxState.isEnabled}
+        isFocused={false}
+        isHovered={false}
+        isChecked={targetCheckboxState.isChecked}
+        clickCallback={() =>
+          setTargetCheckboxState({
+            ...targetCheckboxState,
+            isChecked: !targetCheckboxState.isChecked
+          })
+        }
+      />
+      <Checkbox
+        name="Disable target"
+        isChecked={enableCheckboxState.isChecked}
+        clickCallback={onHandleDisable}
+      />
+      <h2>Unckecked</h2>
+      <Checkbox name="Unchecked Disable" isChecked={false} isEnabled={false} />
+      <h2>Checked</h2>
+      <Checkbox name="Enable" isChecked={true} />
+      <Checkbox name="Checked Disable" isChecked={true} isEnabled={false} />
     </div>
   );
 }
--- src/Checkbox.js
+++ src/Checkbox.js
@@ -5,29 +5,44 @@ import styles from "./styles/Checkbox.module.css";
 export default function Checkbox({
   name,
   isChecked,
-  handlePress,
-  isEnabled=true,
-  isFocused,
-  isHovered
+  clickCallback,
+  isEnabled = true
 }) {
-  const handleClick = () => {
-    if (isEnabled) {
-      handlePress();
+  const setMark = event => {
+    if (
+      isEnabled &&
+      clickCallback &&
+      (event.keyCode === 32 || event.type === "click")
+    ) {
+      // 32 - space
+      clickCallback();
     }
   };
 
   return (
     <span className={styles.container}>
-      <label for={name.toLowerCase()}>
+      <label htmlFor={name.toLowerCase()}>
         <input
+          tabIndex="-1"
           type="checkbox"
           id={name.toLowerCase()}
           name={name.toLowerCase()}
           checked={isChecked}
-          onClick={handleClick}
-          className={styles.checkBox}
-        /> 
-        <span className={`${styles.checkmark} ${isEnabled ? styles.checkmarkEnabled : styles.checkmarkDisabled}`}></span>
+          onClick={setMark}
+        />
+        <span
+          tabIndex="-1"
+          className={`${styles.checkmark} ${
+            isEnabled ? styles.checkmarkEnabled : styles.checkmarkDisabled
+          }`}
+        ></span>
+        <span
+          tabIndex="1"
+          onKeyDown={setMark}
+          className={`${styles.shadowArea} ${
+            isChecked ? styles.shadowAreaMarked : styles.shadowAreaUnmarked
+          }`}
+        ></span>
         {name}
       </label>
     </span>
@@ -37,8 +52,6 @@ export default function Checkbox({
 Checkbox.propTypes = {
   name: PropTypes.string,
   isChecked: PropTypes.bool,
-  handlePress: PropTypes.func,
-  isEnabled: PropTypes.bool,
-  isFocused: PropTypes.bool,
-  isHovered: PropTypes.bool
+  clickCallback: PropTypes.func,
+  isEnabled: PropTypes.bool
 };
